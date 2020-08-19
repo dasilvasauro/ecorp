ea724ae0dab28ecd5b1be8dc52d8cb0a72f4747b
Bruno <b.santos.slm@gmail.com>
2020-08-19 16:05:09 -0300

    Mudança Servicos

--- app/views/servicos.php
+++ app/views/servicos.php
@@ -13,7 +13,7 @@
 	<div class="blog-post">
 		<p class="blog-post-meta">January 1, 2020 by <i style="color: black;">Tyrell Wellick</i></p>
 		<hr>
-		<p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
+		<p>Cummo sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
 		<blockquote>
 			<p>Curabitur blandit tempus porttitor. <strong>Nullam quis risus eget urna mollis</strong> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
 		</blockquote>
